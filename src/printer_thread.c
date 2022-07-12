c7c46795f9c573dcea53bfab7900926c1674086f
Maurycy Jakiel <maurycy.kzmj@gmail.com>
2022-07-12 16:31:09 +0200

    better printing - reduced flicker

--- src/printer_thread.c
+++ src/printer_thread.c
@@ -5,7 +5,7 @@
 extern logger_function* logger;
 
 void print_usage(size_t proc_no, double arr[proc_no + 1]) {
-    puts("\e[1;1H\e[2J");
+    puts("\e[1;1H");
     printf("total CPU usage: %.2f%%\n", arr[0] * 100.0);
     for (size_t i = 1; i < proc_no + 1; ++i) {
         printf("CPU%zu usage: %.2f%%\n", i - 1, arr[i] * 100.0);
@@ -17,6 +17,7 @@ int print_thread(void* arg) {
   State* state = arg;
   size_t i = 0;
   logger("Printer thread started.\n");
+  puts("\e[1;1H\e[2J");
   while(!state->finished) {
     logger("Printer trying (%d)\n", i);
     mtx_lock(&state->proc_stats[i]->mtx);
